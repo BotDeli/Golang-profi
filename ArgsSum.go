8c4fe1725481b2735a3be991dba118a93a7df9ed
BotDeli <124913435+BotDeli@users.noreply.github.com>
2023-09-18 14:03:59 +0500

    Update ArgsSum.go

--- ArgsSum.go
+++ ArgsSum.go
@@ -1,9 +1,10 @@
 package main
-import ("fmt","os")
+import ("fmt","os","strconv")
 func main(){
   var n int
-  for _, n := range os.StdArgs{
-    sum += n
+  for _, n := range os.StdArgs[1:]{
+    num, _ = strconv.Atoi(n)
+    sum += num
   }
   fmt.Println(sum)
 }
